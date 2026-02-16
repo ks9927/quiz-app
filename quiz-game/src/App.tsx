aa387512e3eab83745d35cfded207e37a85d1452
KS <180429671+ks9927@users.noreply.github.com>
2026-02-16 15:41:08 -0500

    added/styled title and start button

--- quiz-game/src/App.tsx
+++ quiz-game/src/App.tsx
@@ -1,33 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className='flex flex-col gap-10'>
+        <h1 className='font-bold text-2xl'>Comics Quiz</h1>
+        <button className='border-2 border-black bg-blue-500 text-white p-2 rounded-xl cursor-pointer'>Start Game</button>
+    </div>
     </>
   )
 }

04a8e0e557a607fc9edb98f78e29a4e1f5a0ab19
KS <180429671+ks9927@users.noreply.github.com>
2026-02-16 16:17:34 -0500

    began card styling and added logic to start button

--- quiz-game/src/App.tsx
+++ quiz-game/src/App.tsx
@@ -1,14 +1,26 @@
-
+import { useState } from 'react'
+import Card from './Card'
 import './App.css'
 
+
 function App() {
-  
+  const [isClicked, setIsClicked] = useState(false);
+  function showCard() {
+    setIsClicked(true);
+  }
 
   return (
     <>
     <div className='flex flex-col gap-10'>
-        <h1 className='font-bold text-2xl'>Comics Quiz</h1>
-        <button className='border-2 border-black bg-blue-500 text-white p-2 rounded-xl cursor-pointer'>Start Game</button>
+        <h1 className='font-bold text-2xl'>
+          Comics Quiz</h1>
+        <button 
+        onClick={showCard}
+        className='
+        {`${isClicked ? hidden: block}`}
+        border-2 border-black bg-blue-500
+         text-white p-2 rounded-xl cursor-pointer'>
+          Start Game</button>
     </div>
     </>
   )
---
+++ quiz-game/src/Card.tsx
@@ -0,0 +1,10 @@
+export default function Card() {
+
+    return (
+        <>
+        <div className="border-4 bg-pink-400">
+
+        </div>
+        </>
+    )
+}

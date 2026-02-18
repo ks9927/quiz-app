8adc3b93b8987b2fbeebdced5cfbc2bddccd59d3
KS <180429671+ks9927@users.noreply.github.com>
2026-02-17 20:11:56 -0500

    added styling to card

--- quiz-game/src/Card.tsx
+++ quiz-game/src/Card.tsx
@@ -2,8 +2,20 @@ export default function Card() {
 
     return (
         <>
-        <div className="border-4 bg-pink-400">
-        <p>This is a card</p>
+        {/* Actual card */}
+        <div className="border-4 border-pink-400 rounded-2xl p-52
+         bg-pink-400 flex flex-col justify-between content-between
+         gap-7 max-h-56">
+
+            {/* Questions */}
+            <div className="bg-pink-300 p-5 rounded-2xl font-semibold">
+            <p>This is a card</p>
+            </div>
+
+            {/* Answers div */}
+            <div className="border-4 p-11">
+            <p>These are some questions</p>
+            </div>
         </div>
         </>
     )
